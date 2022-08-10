0b3ebf664826b5f96f6ecfd51a02b2921d7e5187
Tomek Nowicki <t.nowicki@pollub.pl>
2022-08-10 12:36:55 +0200

    Wykonanie Zadania 2 - wersja 1

---
+++ src/main/java/pwo/LucasGenerator.java
@@ -0,0 +1,28 @@
+package pwo;
+
+import java.math.BigDecimal;
+
+
+public class LucasGenerator {
+    
+    // TO DO
+    
+    public void reset() {
+   
+        // TO DO
+    }
+    
+    public BigDecimal nextTerm() {
+        
+        // TO DO
+        return null;
+    }
+    
+    
+    public BigDecimal getTerm(int i) {
+        
+        // TO DO
+        return null;
+    }
+    
+}
